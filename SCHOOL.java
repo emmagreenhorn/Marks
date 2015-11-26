0da5399402c1844f797e362c7777016883d58df7
Guest User <Guest@stk-cs01-ws11.westlothian.sch.uk>
2015-11-26 01:39:25 -0800

    Sprint 2.2

---
+++ SCHOOL.java
@@ -0,0 +1,31 @@
+
+public class SCHOOL
+{
+    //array of PUPIL objects
+    private SCHOOL PupilList[];
+    //number of pupils to be called
+    int noOfPupils;
+    public SCHOOL()
+    {
+
+    }
+
+    public void processPupils()
+    {
+        setUpPupilList();
+        countPupilmark();
+       
+        
+    }
+
+    private void setUpPupilList()
+    {
+        //placeholder
+
+    } 
+    
+    public void countOKMark()
+    {
+        //placeholder
+    }
+} 

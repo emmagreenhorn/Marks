319b97e141e1cd742b89a7dc4b43b21adb585e03
emmagreenhorn <Guest@stk-cs01-ws11.westlothian.sch.uk>
2015-11-30 06:16:48 -0800

    sprint 2.4
    done

--- SCHOOL.java
+++ SCHOOL.java
@@ -1,31 +1,49 @@
-
+import javax.swing.JOptionPane;
+import java.io.*;
 public class SCHOOL
 {
-    //array of PUPIL objects
+    
     private SCHOOL PupilList[];
-    //number of pupils to be called
+    
     int noOfPupils;
+    FILEREADCSV PupilFile;
+   
+    
+    
+    
     public SCHOOL()
-    {
+    { 
+        PupilFile = new FILEREADCSV();
 
     }
 
-    public void processPupils()
+    public void processPupils()  throws IOException
     {
         setUpPupilList();
-        countPupilmark();
+        
+        
+        
        
         
     }
 
-    private void setUpPupilList()
+    private void setUpPupilList() throws IOException
     {
-        //placeholder
-
-    } 
+        //first user message
+        System.out.println("ScotFit SCHOOL: Membership pupil update");
+        System.out.println("** Preparing to read data file.");
+        
+        //read file, fetch data as String array contaianing the rows
+        String[] dataRows = PupilFile.readCSVtable();
+        //calculate the number of member rows, skip headings
+        noOfPupils = dataRows.length - 1;
+        //update user with number of rows with membership details
+        System.out.println("**" + noOfPupils + " rows read.\n\n");
+    }
+        
     
-    public void countOKMark()
+    public void countOKmark()
     {
         //placeholder
     }
-} 
+}
